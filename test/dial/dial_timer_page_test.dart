93a7a29ab9b89b29ebee060beffac187ac197a49
JOJO <2683553790qsh@gmail.com>
2026-05-31 21:45:59 +0800

    test: cover dial edge dragging

--- test/dial/dial_timer_page_test.dart
+++ test/dial/dial_timer_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:simple_pomodoro/src/dial/dial_geometry.dart';
 import 'package:simple_pomodoro/src/dial/dial_painter.dart';
 import 'package:simple_pomodoro/src/dial/dial_timer_page.dart';
 import 'package:simple_pomodoro/src/feedback/feedback_service.dart';
@@ -54,5 +55,31 @@ void main() {
     await tester.pump();
 
     expect(findDialPaint(), findsOneWidget);
+  });
+
+  testWidgets('dragging the active edge updates selected minutes', (
+    tester,
+  ) async {
+    final feedbackService = FakeFeedbackService();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: DialTimerPage(feedbackService: feedbackService),
+      ),
+    );
+
+    const geometry = DialGeometry();
+    final dialFinder = findDialPaint();
+    final dialTopLeft = tester.getTopLeft(dialFinder);
+    final dialSize = tester.getSize(dialFinder);
+    final start = dialTopLeft + geometry.pointForMinutes(dialSize, 5);
+    final end = dialTopLeft + geometry.pointForMinutes(dialSize, 15);
+
+    await tester.dragFrom(start, end - start);
+    await tester.pump();
+
+    final customPaint = tester.widget<CustomPaint>(dialFinder);
+    final painter = customPaint.painter! as DialPainter;
+    expect(painter.visualMinutes, greaterThan(5));
+    expect(feedbackService.selections, greaterThan(0));
   });
 }
