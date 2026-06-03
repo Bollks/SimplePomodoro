08e209fb3434d0c6d7d08c520a2d140ab21ee5c8
JOJO <2683553790qsh@gmail.com>
2026-06-03 17:21:11 +0800

    fix: mark completion vibration as alarm feedback

--- android/app/src/main/kotlin/com/bollks/simple_pomodoro/MainActivity.kt
+++ android/app/src/main/kotlin/com/bollks/simple_pomodoro/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.bollks.simple_pomodoro
 
 import android.media.RingtoneManager
+import android.media.AudioAttributes
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
     private val channelName = "simple_pomodoro/feedback"
+    private val logTag = "PomodoroFeedback"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -38,6 +41,8 @@ class MainActivity : FlutterActivity() {
     }
 
     private fun vibrateCompletion() {
+        Log.d(logTag, "vibrateCompletion invoked")
+
         val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val manager = getSystemService(VIBRATOR_MANAGER_SERVICE) as VibratorManager
             manager.defaultVibrator
@@ -53,7 +58,14 @@ class MainActivity : FlutterActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val timings = longArrayOf(0, 180, 80, 260, 80, 420)
             val amplitudes = intArrayOf(0, 220, 0, 255, 0, 255)
-            vibrator.vibrate(VibrationEffect.createWaveform(timings, amplitudes, -1))
+            val attributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+            vibrator.vibrate(
+                VibrationEffect.createWaveform(timings, amplitudes, -1),
+                attributes,
+            )
         } else {
             @Suppress("DEPRECATION")
             vibrator.vibrate(longArrayOf(0, 180, 80, 260, 80, 420), -1)
